df85e91872c6900f460d90af4dc071ef3be03b4e
Rich Carpenter <7009338+artfulbodger@users.noreply.github.com>
2026-09-12 17:13:51 +0100

    add(webmin): added webmin module

---
+++ src/modules/webmin/filesystem/etc/webmin/custom/1591302971.cmd
@@ -0,0 +1,4 @@
+wget http://localhost/?ACLReset=$COMP -O emergencyOpen.html && rm emergencyOpen.html
+Unlock Event
+pi 0 0 0 0 0 0 0 -
+COMP:0::0,1:Tournament Code
---
+++ src/modules/webmin/filesystem/etc/webmin/custom/1591896987.cmd
@@ -0,0 +1,4 @@
+sudo systemctl $action Timing.service
+Timing System
+root 0 1 0 0 0 0 0 -
+action:9:printf "start\nstop" |:0,1:Start or stop
